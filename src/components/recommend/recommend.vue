f253bcdc4e18eb773632134aa94299c5b5310eba
kezhian <1486051473@qq.com>
2019-01-04 18:17:05 +0800

    章节4.11学习

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="recommend" ref="recommend">
-    <scroll  ref="scroll" class="recommend-content" :data="discList">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
-      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-        <div class="slider-content">
-          <slider ref="slider">
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl"/>
-              </a>
-            </div>
-          </slider>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <div class="slider-content">
+            <slider ref="slider">
+              <div v-for="item in recommends">
+                <a :href="item.linkUrl">
+                  <img @load="loadImage" :src="item.picUrl"/>
+                </a>
+              </div>
+            </slider>
+          </div>
         </div>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" class="item">
-            <div class="icon">
-              <img width="60" height="60" :src="item.imgurl"/>
-            </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
-      </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl"/>
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </scroll>
   </div>
@@ -50,6 +50,14 @@
       this._getDiscList()
     },
     methods: {
+      loadIamge() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+          setTimeout(()=>{
+            this.$refs.scroll.refresh()
+          },20)
+        }
+      },
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
